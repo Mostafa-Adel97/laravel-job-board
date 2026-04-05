cfe4cb4e1b2e2e8a16ecf9c146ff3c32edd5b4c9
mostafa adel <mostafaadelelsisi@gmail.com>
2026-04-05 22:31:58 +0200

    Blade Templating Engine Module

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<x-layout :title="$pageTitle">
+  <h2> welcom to my page</h2>
+  <ul>
+    <li>job of today</li>
+    <li>job trending</li>
+  </ul>
+</x-layout>
--- resources/views/job/index.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <h1>Job Board</h1>
-    <h3> My name is {{ $name }} </h3>
-    @foreach ($jobs as $job)
-      <div> {{ $job ['title'] }} : {{ $job ['Salary'] }}</div>
-    @endforeach
-</div>
